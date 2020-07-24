32e120f1909527d87a3c390680af89aeba62822e
KKOPTTARR <2540121298@qq.com>
2020-07-24 14:30:45 +0800

    test

---
+++ idea/TestDemo/20200724/src/Test.java
@@ -0,0 +1,30 @@
+import javax.imageio.stream.ImageInputStream;
+
+/**
+ * @author :大大怪将军
+ * @description :Test
+ * @data : 2020/7/24 9:39
+ */
+
+//逆序输出
+public class Test {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        swap(arr1);
+        for(int n=0;n<arr1.length;n++) {
+            System.out.print(arr1[n]);
+        }
+    }
+
+    public static void swap(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
