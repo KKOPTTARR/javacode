420879b4b632b8348751b4ec89f3130737d1adbc
KKOPTTARR <2540121298@qq.com>
2020-07-25 17:34:36 +0800

    test

--- idea/TestDemo/20200724/src/Test.java
+++ idea/TestDemo/20200724/src/Test.java
@@ -6,7 +6,32 @@ import javax.imageio.stream.ImageInputStream;
  * @data : 2020/7/24 9:39
  */
 
+
+public class Test {
+    
+}
+
+/*
+class Test1 {
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Test1 test=null;
+        test.hello();
+    }
+}
+//输出 hello
+*/
+
+
+
+
 //逆序输出
+/*
 public class Test {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4};
@@ -27,4 +52,4 @@ public class Test {
             j--;
         }
     }
-}
+}*/
