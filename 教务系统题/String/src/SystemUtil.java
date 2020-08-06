4f593d90f5bbdb28298417b0cd83f66d1377cf35
KKOPTTARR <2540121298@qq.com>
2020-08-06 22:37:31 +0800

    test

---
+++ 教务系统题/String/src/SystemUtil.java
@@ -0,0 +1,9 @@
+public class SystemUtil {
+    public static boolean isAdmin(String userId){
+        return userId.toLowerCase()=="admin";
+    }
+    public static void main(String[] args){
+        System.out.println(isAdmin("Admin"));
+    }
+}
+//false
