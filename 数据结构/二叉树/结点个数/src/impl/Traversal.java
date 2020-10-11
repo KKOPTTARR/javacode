0de163bf70f1afbb6758bf4c8899e449c1fbdc4a
KKOPTTARR <2540121298@qq.com>
2020-10-11 23:05:58 +0800

    test

---
+++ 数据结构/二叉树/结点个数/src/impl/Traversal.java
@@ -0,0 +1,44 @@
+package impl;
+
+public class Traversal {
+    public static void preOrder(TreeNode root) {
+        if (root != null) {
+            System.out.printf("%c", root.v);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    public static void inOrder(TreeNode root) {
+        if (root != null) {
+            inOrder(root.left);
+            System.out.printf("%c", root.v);
+            inOrder(root.right);
+        }
+    }
+
+    public static void postOrder(TreeNode root) {
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.printf("%c", root.v);
+        }
+    }
+
+    public static void preOrder2(TreeNode root) {
+        if (root != null) {
+            System.out.printf("%c ", root.v);
+            System.out.print(" {");
+            preOrder2(root.left);
+            System.out.print("} { ");
+            preOrder2(root.right);
+            System.out.print("}");
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode a = BuildTree.buildTree1();
+
+        preOrder2(a);
+    }
+}
