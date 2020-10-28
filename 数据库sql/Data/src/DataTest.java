42acfffd874dd0f1759599257642877bb4e9f4dc
KKOPTTARR <2540121298@qq.com>
2020-10-28 21:41:09 +0800

    test

---
+++ 数据库sql/Data/src/DataTest.java
@@ -0,0 +1,8 @@
+public class DataTest {
+    public static void main(String[] args) {
+
+        //传入long类型数字：表示1970年经过10000毫秒后到达的时间
+        java.util.Data d = new java.util.Data(10000);
+
+    }
+}
