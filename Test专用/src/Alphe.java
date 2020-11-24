6d3a39b8f937524b1bec5b9cb07b27f06c65334c
KKOPTTARR <2540121298@qq.com>
2020-11-24 20:00:44 +0800

    Test

---
+++ Test专用/src/Alphe.java
@@ -0,0 +1,12 @@
+/*
+
+public class Alphe extends Base{
+
+    public static void main(String[] args) {
+        new Alphe();
+        new Base();
+    }
+}
+//打印basebase
+
+*/
---
+++ Test专用/src/Test.java
@@ -0,0 +1,35 @@
+/*
+
+class Base{
+    Base() {
+        System.out.println("base");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
+//接Alphe.java
+
+*/
+
+
+/*
+
+class Tst {
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
+
+public class Test{
+    public static void main(String[] args) {
+        Tst tst = null;
+        tst.hello();
+    }
+}
+//编译正常运行，打印hello
+
+ */
