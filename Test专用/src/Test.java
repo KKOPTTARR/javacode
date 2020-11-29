83f6c49e7078b3e5406300782f04e204678eaec9
KKOPTTARR <2540121298@qq.com>
2020-11-29 20:46:32 +0800

    Test

--- Test专用/src/Test.java
+++ Test专用/src/Test.java
@@ -1,3 +1,54 @@
+
+/*
+
+public class Test{
+
+    public static void main(String[] args) {
+
+        boolean b = true ? false : true == true ? false : true;
+
+        System.out.println(b);
+        //false
+
+
+        //详解：
+
+        //首先拆分式子：
+        //(b = true ? false : true) == (true ? false : true)
+
+        //boolean类型的b默认值false
+
+        //左式：
+        //b等于true吗？等于返回false，不等于返回true
+
+        //右式：
+        //问号左边等于true吗？等于返回false，不等于返回true
+
+    }
+}
+
+*/
+
+
+/*
+public class Test {
+
+    public static void main(String[] args) {
+        int x,y;
+        x=5>>2;
+        //System.out.println(x);
+        //x=
+        y=x>>>2;
+        System.out.println(y);
+        //y=0
+    }
+}
+
+ */
+
+
+
+
 /*
 
 class Base{
@@ -14,6 +65,7 @@ public class Test {
 //接Alphe.java
 
 */
+
 
 
 /*
