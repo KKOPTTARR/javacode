c6888f6f0cce7e9eb3e42e24f11db4323dcccd42
KKOPTTARR <2540121298@qq.com>
2021-01-19 21:09:10 +0800

    test

--- 寒假复习/二叉树前中后序遍历/src/BinaryTree.java
+++ 寒假复习/二叉树前中后序遍历/src/BinaryTree.java
@@ -1,2 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinaryTree {
+    public int[] array={1,2,3,4,5,6,7,8,9};
+    public static List<Node> nodeList = null;
+
+    public void createBinaryTree(){
+
+        nodeList = new LinkedList<Node>();
+
+        for(int nodeIndex=0;nodeIndex<array.length;nodeIndex++){
+            Node start = new Node(array[nodeIndex]);
+            System.out.println("添加的是" + array[nodeIndex]);
+            nodeList.add(start);
+        }
+    }
 }
--- 寒假复习/二叉树前中后序遍历/src/Node.java
+++ 寒假复习/二叉树前中后序遍历/src/Node.java
@@ -3,6 +3,16 @@ public class Node {
     Node rightChild;
     int data;
 
-    
+    public Node(Node leftChild,Node rightChild,int data){
+        this.leftChild=leftChild;
+        this.rightChild=rightChild;
+        this.data= data;
+    }
+
+    public Node(int data){
+        this.data=data;
+        this.leftChild=null;
+        this.rightChild=null;
+    }
 
 }
