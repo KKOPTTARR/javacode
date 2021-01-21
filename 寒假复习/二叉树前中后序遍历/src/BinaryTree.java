6d2755ef69b9cfb4cb093ac7a5916e46a18ed700
KKOPTTARR <2540121298@qq.com>
2021-01-21 23:53:10 +0800

    test

--- 寒假复习/二叉树前中后序遍历/src/BinaryTree.java
+++ 寒假复习/二叉树前中后序遍历/src/BinaryTree.java
@@ -15,6 +15,28 @@ public class BinaryTree {
             nodeList.add(start);
         }
 
-        for()
+        for(int parentIndex=0;parentIndex<array.length/2-1;parentIndex++){
+            nodeList.get(parentIndex).leftChild = nodeList.get(parentIndex*2+1);
+            nodeList.get(parentIndex).rightChild = nodeList.get(parentIndex*2+2);
+        }
+
+        int lastParentIndex = array.length/2-1;
+        nodeList.get(lastParentIndex).leftChild = nodeList.get(lastParentIndex*2+1);
+        if(array.length%2==1){
+            nodeList.get(lastParentIndex).rightChild= nodeList.get(lastParentIndex*2+2);
+        }
+    }
+
+    public static void preOrderTraverse(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.println(node.data + " ");
+        preOrderTraverse(node.leftChild);
+        preOrderTraverse(node.rightChild);
+    }
+
+    public static void midOrderTraverse(Node node){
+
     }
 }
