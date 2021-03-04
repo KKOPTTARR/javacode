80aecf186b38f133d8c55d54d43b67b651a8c019
KKOPTTARR <2540121298@qq.com>
2021-03-04 23:08:37 +0800

    test

---
+++ 2021学期/预研阶段学习/src/v1/HTTPServer.java
@@ -0,0 +1,33 @@
+package v1;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HTTPServer {
+//    public static void main(String[] args) throws IOException {
+//        ServerSocket serverSocket = new ServerSocket(8080);
+//        while (true) {
+//            Socket socket = serverSocket.accept();
+//
+//            Runnable task = new RequestResponseTask(socket);
+//            new Thread(task).start();
+//        }
+//    }
+
+    public static void main(String[] args) throws IOException {
+        // 采用固定大小线程池
+        // 不是最优的方式，但简单
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        ServerSocket serverSocket = new ServerSocket(8080);
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            Runnable task = new RequestResponseTask(socket);
+            threadPool.execute(task);
+        }
+    }
+}
