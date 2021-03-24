7641f559bced064eb98ca3beeb364806952773e9
KKOPTTARR <2540121298@qq.com>
2021-03-24 08:13:06 +0800

    test

---
+++ 寒假复习/二叉树/src/BinaryTree1.java
@@ -0,0 +1,35 @@
+
+//声明二叉树节点类
+class TreeNode {
+    int value;
+    TreeNode left_Node;
+    TreeNode right_Node;
+
+    public TreeNode(int value){
+        this.value=value;
+        this.left_Node=null;
+        this.left_Node=null;
+    }
+}
+
+//声明二叉树类
+class BinaryTree {
+    public TreeNode root;//根节点
+
+    //构造函数：利用传入一个数组的参数来建立二叉树
+    public BinaryTree(int[] data) {
+        for(int i=0;i<data.length;i++) {
+            Add(data[i]);
+        }
+    }
+
+    //将指定的值加入到二叉树中合适的节点
+    public void Add(int value) {
+        TreeNode currentNode = root;
+    }
+
+}
+
+
+public class BinaryTree1 {
+}
