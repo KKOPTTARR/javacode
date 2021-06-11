51f78f2758a7f69a80c8efbe44a53380580e7c63
KKOPTTARR <2540121298@qq.com>
2021-06-11 23:29:37 +0800

    list初始化

---
+++ 每日一练/list初始化/src/test.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        //method one
+        List<String> stringList1 = new LinkedList<String>(){
+            {
+            add("a");
+            add("b");
+            add("c");
+            }
+        };
+
+        //method two
+        List<String> stringList2 = new LinkedList<>();
+        stringList2.add("a");
+        stringList2.add("b");
+        stringList2.add("c");
+
+        //method three
+        List<String> stringList3 = Arrays.asList("a", "b", "c");
+
+    }
+}
